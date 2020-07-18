dc28a2aa4e3973cec936e756fdcbcd3d75a8a077
Artem <somersaultbreaking@gmail.com>
2020-07-18 12:41:30 +0300

    Test animation.

--- Game/AlienController.cs
+++ Game/AlienController.cs
@@ -4,8 +4,38 @@ namespace ByteCrusher.Game
 {
   public class AlienController : IEntityController
   {
+    private MovementDirection _direction;
+    
     public void Process(Scene scene, Entity entity)
     {
+      if (_direction == MovementDirection.Right)
+      {
+        entity.Position.X += 1;
+        _direction = MovementDirection.Bottom;
+      }
+      else if (_direction == MovementDirection.Bottom)
+      {
+        entity.Position.Y -= 1;
+        _direction = MovementDirection.Left;
+      }
+      else if (_direction == MovementDirection.Left)
+      {
+        entity.Position.X -= 1;
+        _direction = MovementDirection.Top;
+      }
+      else if (_direction == MovementDirection.Top)
+      {
+        entity.Position.Y += 1;
+        _direction = MovementDirection.Right;
+      }
+    }
+
+    private enum MovementDirection
+    {
+      Right,
+      Bottom,
+      Left, 
+      Top
     }
   }
 }
