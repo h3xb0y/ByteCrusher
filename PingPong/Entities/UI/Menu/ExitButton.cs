d4d530f6ec4be5f53b9fbd5bfd801bc53c852878
Artem <somersaultbreaking@gmail.com>
2020-09-03 00:39:51 +0300

    [PingPong] Elements drawing.

--- PingPong/Entities/UI/Menu/ExitButton.cs
+++ PingPong/Entities/UI/Menu/ExitButton.cs
@@ -1,19 +1,52 @@
+using ByteCrusher.Addition;
+using ByteCrusher.Data;
 using ByteCrusher.Entities;
 
 namespace PingPong.Entities.UI.Menu
 {
   public class ExitButton : Entity
   {
+    private readonly ExitDrawer _exitDrawer;
+    
     private bool _isSelected;
-
     public bool IsSelected
     {
       set => _isSelected = value;
     }
-    
-    public override IEntityDrawer Drawer()
+
+    public ExitButton()
     {
-      throw new System.NotImplementedException();
+      _exitDrawer = new ExitDrawer();
+      
+      Position = new Position
+      {
+        X = 15,
+        Y = 2
+      };
     }
+
+    public override IEntityDrawer Drawer()
+      => _exitDrawer;
+  }
+
+  internal class ExitDrawer : IEntityDrawer
+  {
+    private readonly AsciiCode _asciiCode;
+    public ExitDrawer()
+    {
+      _asciiCode = new AsciiCode()
+        .AddColor("E", "100", "90")
+        .AddColor("X", "100", "90")
+        .AddColor("I", "100", "90")
+        .AddColor("T", "100", "90");
+    }
+    
+    public string Code()
+      => "EXIT ";
+
+    public string Replace(string element)
+      => _asciiCode
+        .AddDrawing(element)
+        .Build();
   }
 }
--- PingPong/Entities/UI/Menu/StartButton.cs
+++ PingPong/Entities/UI/Menu/StartButton.cs
@@ -1,19 +1,44 @@
+using ByteCrusher.Addition;
 using ByteCrusher.Entities;
 
 namespace PingPong.Entities.UI.Menu
 {
   public class StartButton : Entity
   {
+    private readonly IEntityDrawer _textDrawer;
+    
     private bool _isSelected;
-
     public bool IsSelected
     {
       set => _isSelected = value;
     }
+    
+    public StartButton()
+      => _textDrawer = new ButtonDrawer();
 
     public override IEntityDrawer Drawer()
+      => _textDrawer;
+  }
+
+  internal class ButtonDrawer : IEntityDrawer
+  {
+    private readonly AsciiCode _asciiCode;
+    public ButtonDrawer()
     {
-      throw new System.NotImplementedException();
+      _asciiCode = new AsciiCode()
+        .AddColor("S", "100", "90")
+        .AddColor("T", "100", "90")
+        .AddColor("A", "100", "90")
+        .AddColor("R", "100", "90")
+        .AddColor("T", "100", "90");
     }
+    
+    public string Code()
+      => "START ";
+
+    public string Replace(string element)
+      => _asciiCode
+        .AddDrawing(element)
+        .Build();
   }
 }
