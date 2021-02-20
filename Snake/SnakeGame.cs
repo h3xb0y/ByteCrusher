f8fa599bb682a1bf21b4474698e58ffec9898e59
h3xb0y <somersaultbreaking@gmail.com>
2021-02-20 16:39:19 +0200

    [Snake] Draft project.

---
+++ Snake/SnakeGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ByteCrusher.Entities;
+using Snake.Log;
+
+namespace Snake
+{
+  internal class SnakeGame
+  {
+    public static Game Instance { get; private set; }
+    
+    public static void Main(string[] args)
+    {
+      Instance = new Game(new FileLogger())
+        .AddScenes(Scenes())
+        .SetWidthAndHeight(100, 20);
+
+      Instance.Play();
+    }
+    
+    public static void Stop()
+      => Environment.Exit(0);
+
+    private static IEnumerable<Scene> Scenes()
+    {
+      yield return null;
+    }
+  }
+}
